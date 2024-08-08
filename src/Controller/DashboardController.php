67772715190535cd786ea0e3c46b4dbedb8a1347
Adrien Schroedel <adrien.schroedel91@gmail.com>
2024-08-08 16:25:51 +0200

    installed twig support + twig show start page

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Meeting;
 
 class DashboardController extends AbstractController
 {
@@ -19,12 +22,24 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/dashboard/{meeting_id}', name: 'app_dashboard_show')]
-    public function show($meeting_id): Response
+    public function show(EntityManagerInterface $entityManager, $meeting_id): Response
     {
+        // Get the meeting with the given ID (corresponds to the meeting_id in the database)
+        $meeting = $entityManager
+            ->getRepository(Meeting::class)
+            ->findOneBy([ 'meetingId' => $meeting_id ]);
+
+        // If the meeting doesn't exist, return a 404 error
+        if (!$meeting) {
+            throw $this->createNotFoundException('The meeting does not exist');
+        }
+
+        // Find all the associated courses for the meeting
+        
+
         return $this->render("dashboard/show.html.twig",
             [
-                'message' => $meeting_id,
-                'path' => 'src/Controller/DashboardController.php',
+                'meeting' => $meeting,
             ]);
     }
 }
