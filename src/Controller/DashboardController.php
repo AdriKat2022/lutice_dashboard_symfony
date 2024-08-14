e195b6280372eae07ea382a610a2b4bfd23a845e
Adrien Schroedel <adrien.schroedel91@gmail.com>
2024-08-14 19:08:42 +0200

    Added CSV functionality!

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -52,27 +52,37 @@ class DashboardController extends AbstractController
             ->getRepository(Cours::class)
             ->findBy([ 'event' => $meeting->getEvent() ]);
 
-        // $all_emojis = [];
-        // foreach ($all_courses as $course) {
-        //     $all_emojis[$course->getId()] = $course->getEmojis();
-        // }
+        // Prepare the JSON data for the teacher and the courses
+        // We need to do this in order to pass the data to the JavaScript
+        // $json_teacher = [
+        //     'firstname' => $meeting->getEvent()->getTeacher()->getFirstName(),
+        //     'lastname' => $meeting->getEvent()->getTeacher()->getLastName(),
+        //     'starttime' => $meeting->getStartTime(),
+        //     'endtime' => $meeting->getEndTime(),
+        //     'onlineTime' => $meeting->getOnlineTime(),
+        //     'talkTime' => $meeting->getTalkTime(),
+        //     'webcamTime' => $meeting->getWebcamTime(),
+        //     'messageCount' => $meeting->getMessageCount(),
+        //     'emojis' => $meeting->getEmojis(),
+        // ];
 
         $json_courses = array_map(function($course) {
-        return [
+                return [
             'id' => $course->getId(),
-            'onlineTime' => $course->getOnlineTime(),
-            'talkTime' => $course->getTalkTime(),
-            'webcamTime' => $course->getWebcamTime(),
-            'messageCount' => $course->getMessageCount(),
-            'emojis' => $course->getEmojis(),
-        ];
-        }, $all_courses);
+                    'onlineTime' => $course->getOnlineTime(),
+                    'talkTime' => $course->getTalkTime(),
+                    'webcamTime' => $course->getWebcamTime(),
+                    'messageCount' => $course->getMessageCount(),
+                    'emojis' => $course->getEmojis(),
+                ];
+            }, $all_courses);
 
         return $this->render("dashboard/show.html.twig",
             [
-                'json_courses' => $json_courses,
                 'meeting' => $meeting,
                 'all_courses' => $all_courses,
+                // 'json_meeting' => $json_teacher,
+                'json_courses' => $json_courses,
                 'emojis_visual_map' => [
                     'raiseHand' => 'bi bi-person-raised-hand',
                     'happy' => 'bi bi-emoji-smile',
@@ -81,6 +91,92 @@ class DashboardController extends AbstractController
                     'neutral' => 'bi bi-emoji-neutral',
                 ],
             ]);
+    }
+
+
+    #[Route('/dashboard/{meeting_id}/download-csv', name: 'app_dashboard_download_csv')]
+    public function download(EntityManagerInterface $entityManager, $meeting_id): Response
+    {
+        // Get the meeting with the given ID (corresponds to the meeting_id in the database)
+        $meeting = $entityManager
+            ->getRepository(Meeting::class)
+            ->findOneBy([ 'meetingId' => $meeting_id ]);
+
+        // If the meeting doesn't exist, return a 404 error
+        if (!$meeting) {
+            return $this->render("dashboard/not_found.html.twig",
+                [
+                    'meeting_id' => $meeting_id,
+                ]
+        );
+        }
+        
+        // If the dashboard isn't ready (dashboardReady property)
+        if (!$meeting->isDashboardReady()) {
+            return $this->render("dashboard/not_ready.html.twig",
+                [
+                    'meeting' => $meeting,
+                ]);
+        }
+
+        // Find all the associated courses for the meeting
+        $all_courses = $entityManager
+            ->getRepository(Cours::class)
+            ->findBy([ 'event' => $meeting->getEvent() ]);
+        
+        $csv_headers = "First Name, Last Name, First Entered, Last Left, Online Time, Talk Time, Webcam Time, Message Count, Emojis\n";
+
+        // Add the teacher's data to the CSV (which corresponds to the meeting properties)
+        $csv_data = $meeting->getEvent()->getTeacher()->getFirstName() . "," .
+                    $meeting->getEvent()->getTeacher()->getLastName() . "," .
+                    $meeting->getStartTime()->format('Y-m-d H:i:s') . "," .
+                    $meeting->getEndTime()->format('Y-m-d H:i:s') . "," .
+                    $meeting->getOnlineTime() . "," .
+                    $meeting->getTalkTime() . "," .
+                    $meeting->getWebcamTime() . "," .
+                    $meeting->getMessageCount() . "," .
+                    $this->formatEmojisToText($meeting->getEmojis()) . "\n";
+        
+        // Add the courses' data to the CSV
+        foreach ($all_courses as $course) {
+            $csv_data .= $course->getEleve()->getFirstName() . "," .
+                        $course->getEleve()->getLastName() . "," .
+                        $course->getStartTime()->format('Y-m-d H:i:s') . "," .
+                        $course->getEndTime()->format('Y-m-d H:i:s') . "," .
+                        $course->getOnlineTime() . "," .
+                        $course->getTalkTime() . "," .
+                        $course->getWebcamTime() . "," .
+                        $course->getMessageCount() . "," .
+                        $this->formatEmojisToText($course->getEmojis() ). "\n";
+        }
+
+
+        $csv_content = $csv_headers . $csv_data;
+
+        // Return the CSV file as a response
+        return new Response(
+            $csv_content,
+            200,
+            [
+                'Content-Type' => 'text/csv',
+                'Content-Disposition' => 'attachment; filename="'. $meeting->getMeetingName() .'.csv"',
+            ]
+        );
+    }
+
+    public function formatEmojisToText(?array $emojis): string
+    {
+        if (!$emojis) {
+            return "";
+        }
+        
+        $emojis_text = "";
+        foreach ($emojis as $emoji_name => $emoji) {
+            $emojis_text .= $emoji_name . ":" . $emoji['count'] . " ";
+        }
+        $emojis_text = rtrim($emojis_text, " ");
+
+        return $emojis_text;
     }
 
     // public function exportUsers() {
